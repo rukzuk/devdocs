b9fba2838795ed6e741f4964429845872703d23b
Karl Glatz <karl.glatz@rukzuk.com>
2014-07-07 19:35:17 +0200

    removed unused theme files

--- install/gendoc.sh
+++ install/gendoc.sh
@@ -9,7 +9,7 @@ function sami_create_docu() {
     then
         php vendor/bin/sami.php update "$sami_config_file"
 
-		sami_files=( "build/php/Render/APIs/APIv1/CSSAPI.html" "build/php/Render/APIs/APIv1/HeadAPI.html" "build/php/Render/APIs/APIv1/MediaIcon.html" "build/php/Render/APIs/APIv1/MediaImage.html" "build/php/Render/APIs/APIv1/MediaItem.html" "build/php/Render/APIs/APIv1/Navigation.html" "build/php/Render/APIs/APIv1/Page.html" "build/php/Render/APIs/APIv1/RenderAPI.html" "build/php/Render/APIs/RootAPIv1/RootCssAPI.html" "build/php/Render/APIs/RootAPIv1/RootRenderAPI.html" )
+		sami_files=( "build/php/Render/APIs/APIv1/CSSAPI.rst" "build/php/Render/APIs/APIv1/HeadAPI.rst" "build/php/Render/APIs/APIv1/MediaIcon.rst" "build/php/Render/APIs/APIv1/MediaImage.rst" "build/php/Render/APIs/APIv1/MediaItem.rst" "build/php/Render/APIs/APIv1/Navigation.rst" "build/php/Render/APIs/APIv1/Page.rst" "build/php/Render/APIs/APIv1/RenderAPI.rst" "build/php/Render/APIs/RootAPIv1/RootCssAPI.rst" "build/php/Render/APIs/RootAPIv1/RootRenderAPI.rst" )
 		destin=( "src/sphinx/module_tutorial/php_api/php_css_api.rst" "src/sphinx/module_tutorial/php_api/php_head_api.rst" "src/sphinx/module_tutorial/php_api/php_mediaicon_api.rst" "src/sphinx/module_tutorial/php_api/php_mediaimage_api.rst" "src/sphinx/module_tutorial/php_api/php_mediaitem_api.rst" "src/sphinx/module_tutorial/php_api/php_navigation_api.rst" "src/sphinx/module_tutorial/php_api/php_page_api.rst" "src/sphinx/module_tutorial/php_api/php_render_api.rst" "src/sphinx/module_tutorial/php_api/php_root_css_api.rst" "src/sphinx/module_tutorial/php_api/php_root_render_api.rst" )
 
 		for (( i = 0 ; i < ${#sami_files[@]} ; i++ ))
